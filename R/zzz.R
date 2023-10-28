f86d8cc9532554e255c97d78b536a058eb3f3bad
Adrien Le Guillou <git@aleguillou.org>
2023-10-28 11:13:55 +0200

    update readme

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,2 @@
 #' @importFrom dplyr .data
 NULL
-
-if (FALSE) {
-
-  # make and export the render function
-  # ggplot2 and rmarkdown as suggests
-
-  assessments <- readRDS("./assessments.rds")
-  rmarkdown::render(
-    system.file("rmd/assessment.Rmd", package = "swfcalib"),
-    params = list(assessments = assessments)
-  )
-
-}
