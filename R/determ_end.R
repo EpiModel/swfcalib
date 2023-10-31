cfd0c44fd95bba867618ddcfe00389aacc8c1ae6
Adrien Le Guillou <git@aleguillou.org>
2023-10-31 11:48:04 +0100

    export new funs

--- R/determ_end.R
+++ R/determ_end.R
@@ -1,60 +1,4 @@
 #' @export
-determ_poly_end <- function(threshold, poly_n = 3) {
-  force(threshold)
-  force(poly_n)
-  function(calib_object, job, results) {
-    values <- results[[job$targets]]
-    params <- results[[job$params]]
-    target <- job$targets_val
-
-    mscale <- function(x, val) (x - mean(val)) / sd(val)
-    munscale <- function(x, val) x * sd(val) + mean(val)
-
-    complete_rows <- vctrs::vec_detect_complete(values)
-    values <- values[complete_rows]
-    params <- params[complete_rows]
-
-    s_v <- mscale(values, values)
-    s_t <- mscale(target, values)
-    s_p <- mscale(params, params)
-
-    mod <- lm(s_v ~ poly(s_p, poly_n))
-    loss_fun <- function(par)  abs(predict(mod, data.frame(s_p = par)) - s_t)
-    predicted_param <- optimize(interval = range(s_p), f = loss_fun)
-
-    s_newp <- predicted_param$minimum
-    s_newv <- predict(mod, data.frame(s_p = s_newp))
-
-    newp <- munscale(s_newp, params)
-
-    old_sideload <- swfcalib::load_sideload(calib_object, job)
-
-    new_sideload <- list(center = newp, shrink = TRUE)
-    swfcalib::save_sideload(calib_object, job, new_sideload)
-
-    if (is.null(old_sideload)) return(NULL)
-    oldp <- old_sideload$center
-
-    s_oldp <- mscale(oldp, params)
-    s_oldv <- predict(mod, data.frame(s_p = s_oldp))
-
-    newv <- munscale(s_newv, values)
-    oldv <- munscale(s_oldv, values)
-
-    if (abs(oldv - newv) < threshold && abs(newv - target) < threshold) {
-      result <- data.frame(x = newp)
-      new_sideload$shrink <- FALSE
-      swfcalib::save_sideload(calib_object, job, new_sideload)
-      names(result) <- job$params
-      return(result)
-    } else {
-      return(NULL)
-    }
-  }
-}
-
-
-#' @export
 determ_end_thresh <- function(thresholds, n_enough) {
   force(thresholds)
   force(n_enough)
@@ -85,6 +29,7 @@ determ_end_thresh <- function(thresholds, n_enough) {
 
 # can be used for 1 or many params I think
 # save centers in sideload using `list(centers = newp)`
+#' @export
 determ_poly_end <- function(threshold, poly_n = 3) {
   force(threshold)
   force(poly_n)
